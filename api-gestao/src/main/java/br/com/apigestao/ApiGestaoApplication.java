addf39ff8f10f6d8c367f80e2c2382c9e94d3996
miguelamaral254 <miguel.amaral.sales@gmail.com>
2025-06-20 16:12:01 -0300

    inicio projeto

---
+++ api-gestao/src/main/java/br/com/apigestao/ApiGestaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.apigestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGestaoApplication.class, args);
+    }
+
+}
---
+++ api-gestao/src/test/java/br/com/apigestao/ApiGestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apigestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
