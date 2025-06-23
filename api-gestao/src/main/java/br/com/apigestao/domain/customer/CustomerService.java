6d37c2f80787903a6d69ea27cf9208bef15a3017
miguelamaral254 <miguel.amaral.sales@gmail.com>
2025-06-22 21:26:53 -0300

    implementado RequestMdcFilter

--- api-gestao/src/main/java/br/com/apigestao/domain/customer/CustomerService.java
+++ api-gestao/src/main/java/br/com/apigestao/domain/customer/CustomerService.java
@@ -3,43 +3,39 @@ package br.com.apigestao.domain.customer;
 import br.com.apigestao.domain.exceptions.ConflictException;
 import br.com.apigestao.domain.exceptions.InvalidException;
 import br.com.apigestao.domain.exceptions.NotFoundException;
-import jakarta.validation.ConstraintViolation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.AllArgsConstructor;
-import jakarta.validation.Validator;
-
-import java.util.Set;
 import java.util.function.Consumer;
 
 @AllArgsConstructor
 @Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
-    private final Validator validator;
     private final Logger logger = LoggerFactory.getLogger(CustomerService.class);
 
     @Transactional()
     public Customer createCustomer(Customer customer) {
         validateCreate(customer);
         Customer savedCustomer = customerRepository.save(customer);
-        logger.info("Customer with ID: {} saved successfully", customer.getId());
+        logger.info("Customer with ID: {} saved successfully [requestId={}]", customer.getId(), MDC.get("requestId"));
         return savedCustomer;
     }
 
     private void validateCreate(Customer customer) {
         if (customerRepository.existsByCpf(customer.getCpf())) {
-            logger.warn("CPF already exists in the system");
+            logger.error("CPF already exists in the system [requestId={}]", MDC.get("requestId"));
             throw new ConflictException("Customer cpf already exists");
         }
 
         if (customerRepository.existsByEmail(customer.getEmail())) {
-            logger.warn("Email already exists in the system");
+            logger.error("Email already exists in the system [requestId={}]", MDC.get("requestId"));
             throw new ConflictException("Customer email already exists");
         }
     }
@@ -60,7 +56,7 @@ public class CustomerService {
         validateUpdate(customer, mergeNonNull);
         mergeNonNull.accept(customer);
         Customer updatedCustomer = customerRepository.save(customer);
-        logger.info("Customer with ID: {} updated successfully", customer.getId());
+        logger.info("Customer with ID: {} updated successfully [requestId={}]", customer.getId(), MDC.get("requestId"));
         return updatedCustomer;
     }
 
@@ -69,18 +65,18 @@ public class CustomerService {
         mergeNonNull.accept(newCustomer);
 
         if (newCustomer.getName() != null && newCustomer.getName().trim().isEmpty()) {
-            logger.error("Customer name is empty");
+            logger.error("Customer name is empty [requestId={}]", MDC.get("requestId"));
             throw new InvalidException("Customer name is required.");
         }
         if (newCustomer.getEmail() != null &&
                 !newCustomer.getEmail().equals(existingCustomer.getEmail()) &&
                 customerRepository.existsByEmail(newCustomer.getEmail())) {
-            logger.error("Email already exists in the system");
+            logger.error("Email already exists in the system [requestId={}]", MDC.get("requestId"));
             throw new ConflictException("Customer Email already exists.");
         }
 
         if (newCustomer.getCpf() != null && (!newCustomer.getCpf().equals(existingCustomer.getCpf()) && customerRepository.existsByCpf(newCustomer.getCpf()))) {
-            logger.error("CPF already exists in the system");
+            logger.error("CPF already exists in the system [requestId={}]", MDC.get("requestId"));
             throw new ConflictException("Customer CPF already exists.");
         }
     }
@@ -88,7 +84,7 @@ public class CustomerService {
     @Transactional
     public void deleteCustomer(Long id) {
         Customer c = findById(id);
-        logger.info("Customer with ID: {} was successfully deleted", id);
+        logger.info("Customer with ID: {} was successfully deleted [requestId={}]", id, MDC.get("requestId"));
         customerRepository.delete(c);
     }
 
@@ -98,13 +94,13 @@ public class CustomerService {
         Customer c = findById(id);
         validateDisable(c);
         c.setEnabled(false);
-        logger.info("Customer with ID: {} was successfully disabled", id);
+        logger.info("Customer with ID: {} was successfully disabled [requestId={}]", id, MDC.get("requestId"));
         customerRepository.save(c);
     }
 
     private void validateDisable(Customer c) {
         if (!c.getEnabled()) {
-            logger.error("Customer is already disabled in the system");
+            logger.error("Customer is already disabled in the system [requestId={}]", MDC.get("requestId"));
             throw new InvalidException("Customer is already disabled");
         }
     }
--- api-gestao/src/main/java/br/com/apigestao/infrastructure/config/WebConfig.java
+++ api-gestao/src/main/java/br/com/apigestao/infrastructure/config/WebConfig.java
@@ -1,9 +1,19 @@
 package br.com.apigestao.infrastructure.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @Configuration
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean<RequestMdcFilter> loggingFilter() {
+        FilterRegistrationBean<RequestMdcFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RequestMdcFilter());
+        registrationBean.addUrlPatterns("/api/*");
+        return registrationBean;
+    }
 }
